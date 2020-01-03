52359b545fc077515b2ae074deaa8b61f339ac66
Maxeem <jmax.posh@gmail.com>
2020-01-03 16:27:26 +0300

    fix project files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\ASUS\Desktop\zawe\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\xylophone-flutter"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/var/home/max/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/tmp/xylophone-flutter"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\ASUS\Desktop\zawe\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/var/home/max/dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
